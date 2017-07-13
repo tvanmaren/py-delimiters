00a7379a71c50bf2f32cf30044a36da0dae66026
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-07-12 22:12:44 -0600

    :construction: add logic (first attempt)

--- delimiters.py
+++ delimiters.py
@@ -1,7 +1,29 @@
 def delim_check(str):
+    delim_vals = {
+        '(': 0,
+        '{': 0,
+        '[': 0,
+        '<': 0,
+    }
     close_dict = {
         ')': '(',
         ']': '[',
         '>': '<',
         '}': '{',
     }
+    open_list = ['(', '{', '<', '[']
+    last_open = ''
+    for char in str:
+        if char in open_list:
+            delim_vals[char] += 1
+            last_open = char
+        elif char in close_dict:
+            if last_open == close_dict[char]:
+                delim_vals[close_dict[char]] -= 1
+            else:
+                return False
+    for key, val in delim_vals.items():
+        if val != 0:
+            return False
+
+    return True
